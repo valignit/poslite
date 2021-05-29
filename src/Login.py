1a481eeef0482c605e41d6f4b39e114788245d98
mohammed iqbal <s_mohdiqbal@yahoo.com>
2021-05-29 10:27:19 -0600

    login integrated with navigation menu

--- src/Login.py
+++ src/Login.py
@@ -1,7 +1,6 @@
 import PySimpleGUI as sg
 import json
-#import DBManager
-#from DBManager import DBManager as dbManager
+import sys
 from DBOperations import DBOperations
 
 def exitPos():
@@ -24,17 +23,10 @@ dbOperation = DBOperations(db_pos_host,db_pos_port,db_pos_name,db_pos_user,db_po
 
 heading: dict = {'size':(100, 1), 'font':('Helvetica 20 bold'), 'text_color':'blue'}
 btm_btn: dict = {'size':(10, 2), 'font':'Helvetica 11 bold'}
+nvg_btn: dict = {'size':(40, 2), 'font':'Helvetica 13 bold'}
 txt_font: dict = {'font':('Helvetica 11 bold'), 'justification':'right', 'size':(10, 1), 'text_color':'black'}
+input_fld: dict = {'readonly':'True', 'disabled_readonly_text_color':'gray','disabled_readonly_background_color':'gray89','size':(20, 1)}
 
-buttonLayout = [
-                    sg.Column(
-                    [
-                        [
-                            sg.Button('F12\nSign In', key='-LoginOk-', **btm_btn ),
-                            sg.Button('Esc-Exit', key='Exit', **btm_btn)
-                        ]
-                    ],pad=((50, 0), (50, 0),(50,0),(50,0)))
-                ]
 header= [
             sg.Column(
                 [
@@ -53,15 +45,89 @@ footer= [
                 ], size=(400, 90), vertical_alignment='center', justification='center', pad=((50, 0), (60, 0),(20,0),(20,0)))
         ]
 
-input_fld: dict = {'readonly':'True', 'disabled_readonly_text_color':'gray','disabled_readonly_background_color':'gray89','size':(20, 1)}
+buttonLayout = [
+                    sg.Column(
+                    [
+                        [
+                            sg.Button('F12\nSign In', key='-LoginOk-', **btm_btn ),
+                            sg.Button('Esc-Exit', key='Exit', **btm_btn)
+                        ]
+                    ],pad=((50, 0), (50, 0),(50,0),(50,0)))
+                ]
+
+
+def navigation():
+    buttonLayout = [
+                [
+                    sg.Column(
+                        [
+                            [
+                                sg.Image(filename='company-logo.GIF', pad=((30, 0)))
+                            ]
+                        ], size=(320, 90), vertical_alignment='center', justification='center',
+                        pad=((35, 0), (0, 0), (0, 0), (0, 0)))
+                ],
+                [
+                    sg.Button('F1 - Estimate', key='-Estimate-', **nvg_btn,pad=((10, 10), (30, 0),(10,0),(10,0))),
+                ],
+                [
+                    sg.Button('F2 - Invoice', key='-Invoice-', **nvg_btn,pad=((10, 10), (10, 0),(10,0),(10,0))),
+                ],
+                [
+                    sg.Button('F3 - Cash', key='-Cash-', **nvg_btn,pad=((10, 10), (10, 0),(10,0),(10,0))),
+                ],
+                [
+                    sg.Button('F4 - Settings', key='-Settings-', **nvg_btn,pad=((10, 10), (10, 0),(10,0),(10,0))),
+                ],
+                [
+                    sg.Button('Esc - Sign Out', key='-SignOut-', **nvg_btn,pad=((10, 10), (10, 0),(10,0),(10,0))),
+                ],
+                [
+                    sg.Column(
+                        [
+                            [
+                                sg.Image(filename='valign-pos.GIF', pad=((30, 0)))
+                            ]
+                        ], size=(400, 90), vertical_alignment='center', justification='center',
+                        pad=((50, 0), (40, 0), (20, 0), (20, 0)))
+                ]
+    ]
+
+    navigationWindow = sg.Window('Navigation Window', buttonLayout, enable_close_attempted_event=True, no_titlebar=True, keep_on_top=True,size=(350, 500), return_keyboard_events=True, finalize=True)
+
+    navigationWindow.bind('<Escape>', exitPos())
+    navigationWindow.bind('<F1>', '')
+    navigationWindow.bind('<F2>', '')
+    navigationWindow.bind('<F3>', '')
+    navigationWindow.bind('<F4>', '')
+
+    while True:             # Event Loop
+        event, values = navigationWindow.Read()
+        print("event",event)
+        if event == sg.WINDOW_CLOSE_ATTEMPTED_EVENT or event is None or event == sg.WIN_CLOSED:
+            sys.exit(0)
+
+        if event == '-SignOut-' or event == '<Escape>':
+            navigationWindow.hide()
+            window['-LOGINUSER-'].update('')
+            window['-LOGINUSER-'].set_focus()
+            window['-LOGINPWD-'].update('')
+            window.UnHide()
+            break
+
+        if event == '-Invoice-' or event == '<F2>':
+            print('invoice screen')
+    navigationWindow.Close()
+
+
 
 layout = [
     [header],
     [sg.Text(' ', key='ErrMsg', size=(100, 1))],
     [sg.Text('User Name:', **txt_font,pad=((20, 5), (10, 0))),
-     sg.In(key='-LOGINUSER-', pad=((0, 0), (15, 5)), size=(20, 1))],
+     sg.In(key='-LOGINUSER-', focus=True,pad=((0, 0), (15, 5)), size=(20, 4))],
     [sg.Text('Password:',**txt_font ,pad=((20, 5), (10, 0))),
-     sg.In(key='-LOGINPWD-' ,password_char='*', pad=((0, 0), (15, 0)), size=(20, 1))],
+     sg.In(key='-LOGINPWD-' ,password_char='*', pad=((0, 0), (15, 0)), size=(20, 4))],
     [sg.Text('Terminal:',**txt_font ,pad=((20, 5), (10, 0))),
      sg.In(key='-TERMINAL-', default_text=data['terminal_id'],**input_fld , pad=((0, 0), (15, 5)))],
     [buttonLayout],
@@ -72,6 +138,7 @@ window = sg.Window('Login Window', layout, enable_close_attempted_event=True,no_
 
 window.bind('<Escape>', exitPos())
 window.bind('<F12>', login())
+window['-LOGINUSER-'].Widget.config(takefocus=1)
 
 while True:
     event, values = window.read()
@@ -92,7 +159,9 @@ while True:
         db_item_row = dbOperation.verifyUser(uid,pwd)
         if db_item_row is not None:
             print('Login successfull...')
-            break
+            window.hide()
+            navigation()
         else:
             print('Login Failed.....')
             window['ErrMsg'].update('Invalid Credential')
+
